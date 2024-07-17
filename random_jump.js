8d4c9f981e79f7ea338cbd848b56aea5031d96b3
kaitoyo4 <abe.123kai.to@gmail.com>
2024-07-17 16:22:55 +0900

    ランダムジャンプの実装

--- random_jump.js
+++ random_jump.js
@@ -1,7 +1,28 @@
-var mylink = new Array(
+var javascript = new Array(
     "#",
     "#"
-  );
+);
+
+var html = new Array(
+    "#",
+    "#"
+);
+
+var css = new Array(
+    "#",
+    "#"
+);
+
+var ruby = new Array(
+    "#",
+    "#"
+);
+
+var business = new Array(
+    "#",
+    "#"
+);
+
   function random_jump() {
     var i = Math.floor(Math.random() * mylink.length);
     location.href = mylink[i];
