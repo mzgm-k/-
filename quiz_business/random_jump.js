1a92bf77ffc387a7bec0287c3a63a7b33a254311
mzgm-k <mizugami.k.toyo@gmail.com>
2024-07-18 17:20:09 +0900

    ヘッダーにハンバーガーメニューを適用させ、ビジネスマナークイズのファイルに複製

---
+++ quiz_business/random_jump.js
@@ -0,0 +1,29 @@
+var javascript = new Array(
+    "#",
+    "#"
+);
+
+var html = new Array(
+    "#",
+    "#"
+);
+
+var css = new Array(
+    "#",
+    "#"
+);
+
+var ruby = new Array(
+    "#",
+    "#"
+);
+
+var business = new Array(
+    "#",
+    "#"
+);
+
+  function random_jump() {
+    var i = Math.floor(Math.random() * mylink.length);
+    location.href = mylink[i];
+  }
