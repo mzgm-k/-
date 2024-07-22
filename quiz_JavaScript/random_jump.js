521f872ac99909c4c691b8ee4946d9d33365dff6
kaitoyo4 <abe.123kai.to@gmail.com>
2024-07-23 00:30:52 +0900

    リンク設定

--- quiz_JavaScript/random_jump.js
+++ quiz_JavaScript/random_jump.js
@@ -1,6 +1,12 @@
 const javascript = new Array(
-    "#",
-    "#"
+    "quiz/javascript_quiz_1.html",
+    "quiz/javascript_quiz_2.html",
+    "quiz/javascript_quiz_3.html",
+    "quiz/javascript_quiz_4.html",
+    "quiz/javascript_quiz_5.html",
+    "quiz/javascript_quiz_6.html",
+    "quiz/javascript_quiz_7.html",
+    "quiz/javascript_quiz_8.html"
     // 追加した出題ページの相対パスを追加してください。
 );
 
