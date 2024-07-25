208211e4482456eec8c5c1009d78ff6eca4fe733
mzgm-k <mizugami.k.toyo@gmail.com>
2024-07-25 17:57:56 +0900

    Rubyクイズのランダムジャンプリンク設定

--- quiz_Ruby/random_jump.js
+++ quiz_Ruby/random_jump.js
@@ -1,6 +1,9 @@
 const ruby = new Array(
-    "#",
-    "#"
+    "ruby_quiz_1.html",
+    "ruby_quiz_2.html",
+    "ruby_quiz_3.html",
+    "ruby_quiz_4.html",
+    "ruby_quiz_5.html"
     // 追加した出題ページの相対パスを追加してください。
 );
 
